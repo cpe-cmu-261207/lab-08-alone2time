a25156b1d36c6565f078f7b3fdf441e52b0721d0
TonPKG <piyaphat_kh@cmu.ac.th>
2022-09-08 15:01:14 +0700

    Completed

--- pages/index.js
+++ pages/index.js
@@ -18,11 +18,26 @@ export default function Home() {
     //copy from old 2d Array
     const newPixels = CanvasLib.copyCanvas(pixels);
     //your code here
+    newPixels[yPos][xPos] = selColor;
+    setPixels(newPixels);
   };
 
   const clear = () => {
     //your code here
+    setPixels(CanvasLib.createEmptyCanvas());
     //Hint : use CanvasLib.createEmptyCanvas()
+  };
+
+  const rand = () => {
+    const color = CanvasLib.createRandomCanvas();
+    const a = [];
+    for (let i = 0; i < 16; i++) {
+      a.push([]);
+      for (let j = 0; j < 16; j++) {
+        a[i].push(color[Math.floor(Math.random() * color.length)]);
+      }
+    }
+    setPixels(a);
   };
 
   return (
@@ -36,7 +51,9 @@ export default function Home() {
           <button className="btn btn-dark" onClick={clear}>
             Clear
           </button>
-          <button className="btn btn-dark">Random Color</button>
+          <button className="btn btn-dark" onClick={rand}>
+            Random Color
+          </button>
         </div>
       </PainterContext.Provider>
     </div>
